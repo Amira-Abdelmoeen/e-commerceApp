efb1070911ae6e0cba58ef5d43e1ce745745c244
Amira-Abdelmoeen <amiraamur300@gmail.com>
2024-03-04 21:13:13 +0200

    edit

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -11,64 +11,61 @@ import { CartService } from '../cart.service';
 })
 export class WishlistComponent implements OnInit {
 
-  products:Product[]=[]
+  products: Product[] = []
   wishlistData: string[] = []
 
-  constructor(private _WishlistService:WishlistService,private toastEvokeService:ToastEvokeService , private _CartService: CartService) { }
+  constructor(private _WishlistService: WishlistService, private toastEvokeService: ToastEvokeService, private _CartService: CartService) { }
 
   ngOnInit(): void {
 
-    localStorage.setItem("currentPage" , "/wishlist")
+    localStorage.setItem("currentPage", "/wishlist")
     this._WishlistService.getWishListApi().subscribe({
-      next: (res) => 
-      {
+      next: (res) => {
         this.products = res.data
-        const newData = res.data.map( (item:any)=> item._id  )
+        const newData = res.data.map((item: any) => item._id)
         this.wishlistData = newData
-       },
-      error: (err) => {console.log(err) }
+      },
+      error: (err) => { console.log(err) }
     })
 
   }
 
-   addCartBtn(pId:string)
-   {
+  addCartBtn(pId: string) {
     this._WishlistService.addToWhishListApi(pId).subscribe({
-     next: (res) =>{
-       this.toastEvokeService.success('Success', res.message).subscribe();
-       this._CartService.cartItemsNum.next(res.numOfCartItems) 
-       
-     }
-    })
-   
- 
-   }
+      next: (res) => {
+        this.toastEvokeService.success('Success', res.message).subscribe();
+        this._CartService.cartItemsNum.next(res.numOfCartItems)
 
-   addFav(pId:string):void{
+      }
+    })
+
+
+  }
+
+  addFav(pId: string): void {
     this._WishlistService.addToWhishListApi(pId).subscribe({
-     next: (res) => {
-       this.toastEvokeService.success('Success', res.message).subscribe();
-       this.wishlistData = res.data
-   
-        },
-     error: (err) => {console.log(err) }
-    })
-   }
- 
-   removeFav(pId: string):void{
-     this._WishlistService.removeWishListApi(pId).subscribe({
-       next: (res) => 
-       { 
-          this.toastEvokeService.success('Success', res.message).subscribe();
-          this.wishlistData = res.data
+      next: (res) => {
+        this.toastEvokeService.success('Success', res.message).subscribe();
+        this.wishlistData = res.data
 
-          this._WishlistService.getWishListApi().subscribe({
-            next: (res) => {
-              this.products = res.data
-            }
-          })
-       }
-     })
-   }
- 
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
+
+  removeFav(pId: string): void {
+    this._WishlistService.removeWishListApi(pId).subscribe({
+      next: (res) => {
+        this.toastEvokeService.success('Success', res.message).subscribe();
+        this.wishlistData = res.data
+
+        this._WishlistService.getWishListApi().subscribe({
+          next: (res) => {
+            this.products = res.data
+          }
+        })
+      }
+    })
+  }
+
 }
